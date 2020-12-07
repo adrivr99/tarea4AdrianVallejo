ed09c06bf2baa739b1479800ec299ee00dc45d35
adrivr99 <adri.vallejo.99@gmail.com>
2020-12-07 21:29:12 +0100

    Añadir atributos en clase

---
+++ src/main/java/tarea4adrianvallejo/tarea4.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4adrianvallejo;
+
+/**
+ *
+ * @author adriv
+ */
+public class tarea4 {
+    int numero1 = 1;
+    int numero2 = 2;
+    int numero3 = 3;
+}
