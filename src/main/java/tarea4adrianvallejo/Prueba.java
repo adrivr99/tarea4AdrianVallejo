95b3b3262e708ac71a7b96a39998e6302c4f2049
enriquejdd <enriquejdd1999@gmail.com>
2020-12-07 21:52:57 +0100

    Añade objetos y ejemplos

--- src/main/java/tarea4adrianvallejo/Prueba.java
+++ src/main/java/tarea4adrianvallejo/Prueba.java
@@ -11,6 +11,16 @@ package tarea4adrianvallejo;
  */
 public class Prueba {
     public static void main(String[] args) {
+        tarea4 miNumero = new tarea4(15, 20, 35);
+        tarea4 miNumero1 = new tarea4(20, 03, 1999);
+        tarea4 miNumero2 = new tarea4();
         
+        System.out.println("Los valores de miNumero son: " + miNumero.getNumero1() + " " + miNumero.getNumero2() + " " + miNumero.getNumero3());
+        System.out.println("");
+        
+        miNumero1.toString();
+        System.out.println("");
+        
+        System.out.println(miNumero2);
     }
 }
