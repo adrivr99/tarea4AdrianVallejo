1fc183fd5d84c0755528fc4a58b08a1cda50b36d
adrivr99 <adri.vallejo.99@gmail.com>
2020-12-07 21:38:42 +0100

    Añade getter y setter en clase inventada

--- src/main/java/tarea4adrianvallejo/tarea4.java
+++ src/main/java/tarea4adrianvallejo/tarea4.java
@@ -22,6 +22,30 @@ public class tarea4 {
         this.numero2 = numero2;
         this.numero3 = numero3;
     }
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+
+    public int getNumero3() {
+        return numero3;
+    }
+
+    public void setNumero1(int numero1) {
+        this.numero1 = numero1;
+    }
+
+    public void setNumero2(int numero2) {
+        this.numero2 = numero2;
+    }
+
+    public void setNumero3(int numero3) {
+        this.numero3 = numero3;
+    }
     
     
 }
